756409dfc59373b25356e9692674229e29888df2
Walter Williams <walt@sawtoothsoftware.com>
2011-11-14 13:35:43 -0700

    Updated documentation

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,6 +1,20 @@
 <?php
 
 /*
+	Spammer Checker 1.0.0 (c) 2011, Sawtooth Software, Inc.
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/*
 	Plugin Name: Spammer Checker
 	Plugin URI: 
 	Plugin Description: Provides options for checking whether an IP or email is a known spammer
@@ -8,7 +22,7 @@
 	Plugin Date: 2011-11-14
 	Plugin Author: Walter Williams
 	Plugin Author URI: 
-	Plugin License: 
+	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.4
 */
 
